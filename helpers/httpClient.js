caee9fc2e9f9749af27c1edfa2d95255e82e6c28
SwedishHunk <max.dettwiler@gmail.com>
2025-02-10 14:42:56 +0100

    Updated Httpclient with a post mehtid and refactored user logic

--- helpers/httpClient.js
+++ helpers/httpClient.js
@@ -22,4 +22,28 @@ export class HttpClient {
             
         }
     }
+
+    async post(endpoint, data) {
+        try {
+            const response = await fetch (this._url + endpoint, {
+                method: 'POST',
+                headers: {
+                    'x-apikey':'67a9efe8020c064e18e653bd',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw new Error(
+                    `Felkod: ${response.status} Felmeddelande: ${response.statusText}`
+                )
+            }
+        } catch (error) {
+            throw new Error(error);
+            
+        }
+    }
 }
